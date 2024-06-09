52d7e85c7ed01fb8fdca82b929b557eabe76acd9
avilabss <contact@avilabs.net>
2024-06-09 17:13:24 +0530

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-export * from './types/authenticate.js'
+export * from './enums/authenticate.js'
 
 export * from './exceptions/baseError.js'
+
+export * from './types/authenticate.js'
+export * from './types/connection.js'
+export * from './types/state.js'
 
 export * from './utils/connection.js'
 export * from './utils/crypto.js'
 export * from './utils/logger.js'
+export * from './utils/state.js'
